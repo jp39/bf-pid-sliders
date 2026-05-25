6092002dd9d4a91005bba72278410496902c9261
jp39 <jp39@gmx.com>
2026-05-25 16:02:22 +0200

    Add installable PWA support

--- app.js
+++ app.js
@@ -325,3 +325,9 @@ async function copyCli() {
 
 buildControls();
 render();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
